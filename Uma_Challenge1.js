d9c8aca88b2c47c9e7054fd21623d896a7f348b5
Umamaheswar Thimmareddy <umamaheswarreddy.t@gmail.com>
2024-02-06 16:58:16 +0530

    commiting challenge 1

---
+++ Uma_Challenge1.js
@@ -0,0 +1,19 @@
+var myName = 'Uma';
+console.log(myName);
+alert('good morning! Umamaheswar');
+//- You order a T-Shirt for Rs-10, socks for Rs-8, and dinner plates for Rs-20. Use JavaScript to calculate the total cost of your order.
+let tshirt=10;
+let scoks=8;
+let dinnerPlates=20;
+
+let total = tshirt + scoks + dinnerPlates;
+console.log(total);
+
+let accountBalance, lunch, dinner, salay;
+accountBalance=100;
+lunch=-20;
+dinner=-50;
+salay=200;
+
+let remainingBalance = accountBalance + lunch + dinner + salay;
+console.log(remainingBalance);
